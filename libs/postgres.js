aee39816c037eb48c299cd02c9e5d1440d66d500
paula <paulacruz.tg@gmail.com>
2025-06-20 19:17:59 -0500

    Conexión Pool con DATABASE_URL y SSL en producción

--- libs/postgres.js
+++ libs/postgres.js
@@ -1,4 +1,4 @@
-const { Client } = require('pg');
+/*const { Client } = require('pg');
 
 async function getConnection() {
   const client = new Client({
@@ -7,6 +7,21 @@ async function getConnection() {
     user: 'paula',
     password: '123456',
     database: 'my_store',
+  });
+  await client.connect();
+  return client;
+}
+
+module.exports = getConnection;*/
+const { Client } = require('pg');
+
+async function getConnection() {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl:
+      process.env.NODE_ENV === 'production'
+        ? { rejectUnauthorized: false }
+        : false,
   });
   await client.connect();
   return client;
--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg');
+/*const { Pool } = require('pg');
 
 const { config } = require('./../config/config');
 
@@ -7,5 +7,16 @@ const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const pool = new Pool({ connectionString: URI });
+
+module.exports = pool;*/
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : false,
+});
 
 module.exports = pool;
